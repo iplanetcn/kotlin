fa48331da7f3c328b7ab45016e6de8357a7fb878
john <iplanetcn@gmail.com>
2021-12-10 14:28:20 +0800

    update gradle config

--- src/main/kotlin/regex/RegexUtils.kt
+++ src/main/kotlin/regex/RegexUtils.kt
@@ -1,10 +1,11 @@
 package regex
 
-object RegexUtils{
+object RegexUtils {
     // language=RegExp
     private const val REG_EMAIL = "^([a-z0-9_.-]+)@([\\da-z.-]+)\\.([a-z.]{2,6})\$"
 
-    @JvmStatic fun checkEmailIsValid(text: String): Boolean {
+    @JvmStatic
+    fun checkEmailIsValid(text: String): Boolean {
         return Regex(REG_EMAIL).matches(text)
     }
 
